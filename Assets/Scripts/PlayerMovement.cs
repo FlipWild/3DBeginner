526e62db7aecd041615d9bfcba3b9020fb84f71d
DanS <DSumbillo@colum.edu>
2019-11-21 12:33:43 -0600

    Audio
    Added sound effects to John, ghosts, and game ending. Changed necessary scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody m_Rigidbody;
 
+    AudioSource m_AudioSource;
+
     public float turnSpeed = 20f;
 
     // Start is called before the first frame update
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -38,6 +41,18 @@ public class PlayerMovement : MonoBehaviour
         //Rotation
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
     }
 
     //Movement
